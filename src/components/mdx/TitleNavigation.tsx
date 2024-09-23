7017f88bdf69b632785fa0cd94752ef4ce9ff3e9
BaeYeongGi <byg5913@gmail.com>
2024-09-23 16:57:08 +0900

    [add]: 페이지 목차 ui toggle 버튼 적용

--- src/components/mdx/TitleNavigation.tsx
+++ src/components/mdx/TitleNavigation.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { useState } from "react";
 import { useTheme } from "next-themes";
 import styles from '@/src/styles/TitleNavigation.module.scss';
 import { useHeadingObserver } from '@/src/hooks/useHeadingObserver';
+import DownArrow from '@/public/images/svg/DownArrow';
 
 interface TitleNavigationPropsType {
   data?: Array<{
@@ -12,21 +14,26 @@ interface TitleNavigationPropsType {
 
 const TitleNavigation = ({ data }: TitleNavigationPropsType) => {
   const { theme } = useTheme();
+  const [ showTitles, setShowTitles ] = useState(false);
   const activeIdList = useHeadingObserver('h2')
 
+  const toggleTitlesList = () => {
+    setShowTitles(prev => !prev);
+  }
+
   return (
-    <aside className={theme === 'dark' ? `${styles.dark} ${styles.navigation} `: styles.navigation}>
-      <ul>
+    <div className={theme === 'dark' ? `${styles.dark} ${styles.navigation} `: styles.navigation}>
+      <button className={`${styles.page_titles_button} ${showTitles ? styles.active : ''}`} onClick={toggleTitlesList}>페이지 목차 <DownArrow/></button>
+      <ul className={showTitles ? styles.active : ''}>
         {data?.map((item, index) => (
           <li key={index}>
             <a href={item.link} className={activeIdList?.includes(item.link) ? styles.active : ''}>
-              {item.link.replace('#', '')}
+              {item.link.replace('#', '').replaceAll('-', ' ')}
             </a>
           </li>
         ))}        
       </ul>
-
-    </aside>
+    </div>
   );
 };
 
